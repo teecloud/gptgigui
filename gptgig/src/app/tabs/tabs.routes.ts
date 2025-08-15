3b7709a7081f022f44a0c611b6055b0c787c0357
Terrill Edwards <terrilledwards@icloud.com>
2025-08-15 18:39:57 -0400

    stable check

--- gptgig/src/app/tabs/tabs.routes.ts
+++ gptgig/src/app/tabs/tabs.routes.ts
@@ -22,31 +22,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('./pages/search/search.page').then( m => m.SearchPage)
-  },
-  {
-    path: 'services',
-    loadComponent: () => import('./pages/services/services.page').then( m => m.ServicesPage)
-  },
-  {
-    path: 'inbox',
-    loadComponent: () => import('./pages/inbox/inbox.page').then( m => m.InboxPage)
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
-  },
-  {
-    path: 'admin',
-    loadComponent: () => import('./pages/admin/admin.page').then( m => m.AdminPage)
-  },
+  }
 ];

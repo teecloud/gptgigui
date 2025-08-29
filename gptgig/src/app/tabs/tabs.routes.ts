6dc8bddb385043643287ac7e9678485963294b5a
teecloud <150497016+teecloud@users.noreply.github.com>
2025-08-29 10:13:10 -0400

    feat: add inventory tab with product scanning

--- gptgig/src/app/tabs/tabs.routes.ts
+++ gptgig/src/app/tabs/tabs.routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
        { path: 'home', loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage) },
       { path: 'cart', loadComponent: () => import('../cart/cart.page').then(m => m.CartPage) },
       { path: 'services', loadComponent: () => import('./pages/services/services.page').then(m => m.ServicesPage) },
+      { path: 'inventory', loadComponent: () => import('./pages/inventory/inventory.page').then(m => m.InventoryPage) },
       { path: 'inbox', loadComponent: () => import('./pages/inbox/inbox.page').then(m => m.InboxPage) },
       { path: 'social', loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage) },
       { path: 'admin', loadComponent: () => import('./pages/admin/admin.page').then(m => m.AdminPage) },

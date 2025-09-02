49a49823332c5039bc8d57cca7f1a873e72c5be6
teecloud <150497016+teecloud@users.noreply.github.com>
2025-09-02 19:16:59 -0400

    feat: show profile info in toolbar modal

--- gptgig/src/app/components/profile-modal/profile-modal.component.spec.ts
+++ gptgig/src/app/components/profile-modal/profile-modal.component.spec.ts
@@ -2,6 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileModalComponent } from './profile-modal.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { ProfileService } from 'src/app/services/profile.service';
+
+class MockProfileService {
+  getProfile() {
+    return of({ id: 1, displayName: 'Test User' });
+  }
+}
 
 describe('ProfileModalComponent', () => {
   let component: ProfileModalComponent;
@@ -10,6 +18,7 @@ describe('ProfileModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProfileModalComponent, IonicModule.forRoot(), RouterTestingModule],
+      providers: [{ provide: ProfileService, useClass: MockProfileService }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileModalComponent);
--- gptgig/src/app/components/profile-modal/profile-modal.component.ts
+++ gptgig/src/app/components/profile-modal/profile-modal.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Profile, ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-profile-modal',
@@ -10,8 +11,25 @@ import { Router } from '@angular/router';
   templateUrl: './profile-modal.component.html',
   styleUrls: ['./profile-modal.component.scss'],
 })
-export class ProfileModalComponent {
-  constructor(private modalCtrl: ModalController, private router: Router) {}
+export class ProfileModalComponent implements OnInit {
+  profile: Profile | null = null;
+
+  constructor(
+    private modalCtrl: ModalController,
+    private router: Router,
+    private profileService: ProfileService
+  ) {}
+
+  ngOnInit() {
+    this.profileService.getProfile().subscribe({
+      next: (res) => {
+        this.profile = res;
+      },
+      error: () => {
+        this.profile = null;
+      }
+    });
+  }
 
   dismiss() {
     this.modalCtrl.dismiss();

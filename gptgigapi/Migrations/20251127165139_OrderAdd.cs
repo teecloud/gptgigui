5630188c1032581fdb082404ccac3fd6e64e1642
Terrill Edwards <terrilledwards@icloud.com>
2025-11-27 11:02:38 -0600

    add order

---
+++ gptgigapi/Migrations/20251127165139_OrderAdd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gptgigapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
